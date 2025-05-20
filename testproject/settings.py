7a73e28e01d6ebae661a3c0784517d511d472403
MatsushitaKazuma <matsushita.kazuma.1@gmail.com>
2025-05-20 09:48:23 +0900

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%==8k#q(3t7$x*5=507k4yak=)ux66r1#i@sdkqbjae5v255zf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','18.142.128.26']
 
 
 # Application definition
